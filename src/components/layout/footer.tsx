104e4df96e7567cc9719ebdeb3c5b7aba7000ae8
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2025-06-16 20:30:20 -0700

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,41 +3,43 @@ import { TypewriterEffect } from "../ui/typewriter-effect";
 
 const Footer = () => {
   return (
-    <footer className="my-4 border-t w-full max-[800px]:w-[90%] mx-auto">
-      {/* <p className="italic w-full text-center mt-4">
+    <div className="w-full px-6">
+      <footer className="my-4 border-t w-full mx-auto">
+        {/* <p className="italic w-full text-center mt-4">
         made with <span className="not-italic">💙</span> &{" "}
         <span className="not-italic">☕️</span> by{" "}
         <span className="brand-gradient-text">smrth</span>
       </p> */}
-      <TypewriterEffect
-        className="italic w-full text-center mt-4 text-sm"
-        words={[
-          {
-            text: "made",
-          },
-          {
-            text: "with",
-          },
-          {
-            text: "♥",
-          },
-          {
-            text: "&",
-          },
-          {
-            text: "☕️",
-          },
-          {
-            text: "by",
-          },
-          {
-            text: "smrth",
-            className: "font-mono text-blue-400 not-italic",
-          },
-        ]}
-        cursorClassName="-mb-1 w-0.5 bg-blue-400 text-sm h-5"
-      />
-    </footer>
+        <TypewriterEffect
+          className="italic w-full text-center mt-4 text-sm"
+          words={[
+            {
+              text: "made",
+            },
+            {
+              text: "with",
+            },
+            {
+              text: "♥",
+            },
+            {
+              text: "&",
+            },
+            {
+              text: "☕️",
+            },
+            {
+              text: "by",
+            },
+            {
+              text: "smrth",
+              className: "font-mono text-blue-400 not-italic",
+            },
+          ]}
+          cursorClassName="-mb-1 w-0.5 bg-blue-400 text-sm h-5"
+        />
+      </footer>
+    </div>
   );
 };
 
