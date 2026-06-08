26d90c00c3e33566176eecaa3ddd762cb95fdba2
Samarth Chitgopekar <67826352+http-samc@users.noreply.github.com>
2026-06-07 22:47:49 -0400

    Refine garage caption styling (#31)
    * Add flight map component
    
    * Fix pnpm workspace config
    
    * Update next-mdx-remote
    
    * Fix MDX component build defaults
    
    * Restore MDX expression props
    
    * Tighten flight map card layout
    
    * Update X profile link
    
    * Fix flight map card overflow
    
    * Refine flight map alignment
    
    * Adjust flight map header padding
    
    * Add garage MDX component
    
    * Refine garage caption styling

--- src/components/home/garage.tsx
+++ src/components/home/garage.tsx
@@ -15,7 +15,7 @@ const Garage = ({ vehicles }: GarageProps) => {
         <div key={vehicle.name} className="relative w-full h-64 overflow-hidden rounded-lg group/vehicle border">
           <Image src={vehicle.img} alt={vehicle.name} fill className="object-cover sm:grayscale group-hover/vehicle:grayscale-0 transition-all" />
           <div className="absolute inset-0 bg-black/0 flex items-end justify-start pb-2">
-            <p className="text-white line-clamp-1 text-sm font-semibold">{vehicle.name}</p>
+            <p className="text-white/50 group-hover/vehicle:text-white transition-all line-clamp-1 text-sm font-semibold">{vehicle.name}</p>
           </div>
         </div>
       ))}
